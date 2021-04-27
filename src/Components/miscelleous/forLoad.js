ee9cb6486ff44242e2facd24d4aa63ce3a8bec44
Mustafa Mustafa <mamustafa@gtcc.edu>
2021-04-27 13:34:40 -0400

    new btn

--- src/Components/miscelleous/forLoad.js
+++ src/Components/miscelleous/forLoad.js
@@ -4,7 +4,7 @@ import Spinner from './spinner/spinner';
 const forLoad=()=>{
     return(
         <div style={{height:"50vh",width:"100vw",color:"#000",fontWeight:"bold",margin:"0 auto",padding:"0 auto"}}>
-            <h1>2 Brother</h1>
+            <h1>Nobu Qatar</h1>
             <h3>Loading a Delicious Menu</h3>
             <Spinner/>
         </div>
